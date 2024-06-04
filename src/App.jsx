beb6978902375651a6251ac6bf89f39b5966f6e0
LebogangMputle <lebozikalala@gmail.com>
2024-06-04 14:55:55 +0200

    "Added Layout component and wrapped App routes with it"

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./Pages/Home"
 import About from "./Pages/About"
 import Vans from "./Pages/Vans"
 import VanDetail from "./Pages/VanDetail"
-
+import Layout from "./components/Layout"
 import "./server"
 
 
@@ -13,10 +13,12 @@ export default function App() {
     return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/about" element={<About />}/>
-          <Route path="/vans" element={<Vans />}/>
-          <Route path="/vans/:id" element={<VanDetail />}/>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />}/>
+            <Route path="/about" element={<About />}/>
+            <Route path="/vans" element={<Vans />}/>
+            <Route path="/vans/:id" element={<VanDetail />}/>
+          </Route>
         </Routes>
       </BrowserRouter>
     )
