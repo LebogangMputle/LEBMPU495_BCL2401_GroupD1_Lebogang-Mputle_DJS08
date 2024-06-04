45a06ad485a949099b68052b2e0cbf3257dbf249
LebogangMputle <lebozikalala@gmail.com>
2024-06-04 10:07:23 +0200

    A new import bgImg from ../assets/images/about-hero.png was added. A new import Link from react-router-dom was added.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+
+import { Link } from "react-router-dom"
 
 export default function Home() {
     return(
-        <h1>Home</h1>
+        <div className="home-container">
+            <h1>You got travel plans, we got the travel vans.</h1>
+            <p>Add adventures to your life by joining the #VanLife movement. Rent the perfect van to make your perfect road trip.</p>
+            <Link to="vans">Find your van</Link>
+        </div>    
     )
 }
