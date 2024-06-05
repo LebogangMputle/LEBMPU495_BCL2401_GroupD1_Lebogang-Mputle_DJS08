f61c3d98417f7546664ebaa1b99a823658d7825d
LebogangMputle <lebozikalala@gmail.com>
2024-06-05 14:02:36 +0200

    "Updated App.jsx and Dashboard.jsx files, adding imports, routes, and components"

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "./Pages/Home"
-import About from "./Pages/About"
-import Vans from "./Pages/Vans"
-import VanDetail from "./Pages/VanDetail"
+//import React from 'react';
+import { BrowserRouter, Routes, Route,  } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Vans from "./Pages/Vans";
+import VanDetail from "./Pages/VanDetail";
 import Dashboard from './Pages/Host/Dashboard';
 import Income from './Pages/Host/Income';
 import Reviews from './Pages/Host/Income';
-import Layout from "./components/Layout"
+import Layout from "./components/Layout";
+import HostLayout from "./components/HostLayout";
 import "./server"
 
 
@@ -21,9 +22,11 @@ export default function App() {
             <Route path="/about" element={<About />}/>
             <Route path="/vans" element={<Vans />}/>
             <Route path="/vans/:id" element={<VanDetail />}/>
+
+            <Route path="/host" element={<HostLayout />}/>
             <Route path="/host" element={<Dashboard />}/>
             <Route path="/host/income" element={<Income />}/>
-            <Route path="/vans/reviews" element={<Reviews />}/>
+            <Route path="/host/reviews" element={<Reviews />}/>
           </Route>
         </Routes>
       </BrowserRouter>
